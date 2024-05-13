83fd588a3ae00d30c654598673d52ccecfc5b804
AxelMaidana <maidana.axel2003@gmail.com>
2024-05-13 00:01:55 -0300

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss-animated').Config} */
+import animated from 'tailwindcss-animated'
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	plugins: [
+
+	],
+}
